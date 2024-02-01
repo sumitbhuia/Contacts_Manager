a3becd3784988eaf513dda01128db1d0a0ba3365
Sumit <sumitbhuia100@gmail.com>
2024-02-02 01:19:21 +0530

    Initial commit

---
+++ app/src/main/java/com/example/contactsmanager/room/User.kt
@@ -0,0 +1,37 @@
+package com.example.contactsmanager.room
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+/*
+
+    - Setting data class / model class as entity and table name is user
+    - This is a Model class
+    - Used like a template to segregate data
+    - Here we don't put and code except the parameters
+    - @PrimaryKey(autoGenerate = true)  will generate number fo the below parameters
+    - Like indexing the parameters
+    id->1
+    name ->2
+    email->3
+
+  */
+
+@Entity(tableName="user")
+data class User(
+
+    @PrimaryKey(autoGenerate = true)
+
+    // Sets table column(1) name = user_ id
+    @ColumnInfo(name ="user_id")
+    val id : Int ,
+
+    // Sets table column(2) name = user_ name
+    @ColumnInfo(name ="user_name")
+    val name : String ,
+
+    // Sets table column(3) name = user_ email
+    @ColumnInfo(name ="user_email")
+    val email : String){}
---
+++ app/src/main/java/com/example/contactsmanager/room/UserDatabase.kt
@@ -0,0 +1,40 @@
+package com.example.contactsmanager.room
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database (entities = [User::class], version = 1)
+abstract class UserDatabase:RoomDatabase() {
+
+    abstract val userDAO: UserDAO
+
+    companion object {
+        @Volatile
+        // Class to cast into singleton pattern
+        private var INSTANCE: UserDatabase? = null
+
+        // public function to retrieve the singleton instance
+        fun getInstance(context: Context): UserDatabase? {
+
+            synchronized(this) {
+                var instance = INSTANCE
+
+                // Check if the instance is already created
+                if (instance == null) {
+                    // creating database object
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        UserDatabase::class.java, "user_db"
+                    ).build()
+                }
+
+                // return the singleton instance
+                return INSTANCE
+
+            }
+        }
+
+    }
+}
